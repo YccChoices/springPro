205d0f9803974749939b3829541bacbdf9f7bed9
YccChoice <a1358906086@qq.com>
2016-09-12 22:57:28 +0800

    update and correct some miatakes

--- src/test/java/com/ycc/test/TestUser.java
+++ src/test/java/com/ycc/test/TestUser.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.ycc.service.ProductService;
 import com.ycc.service.UserService;
 
 public class TestUser {
@@ -29,5 +30,11 @@ public class TestUser {
 		UserService service = (UserService) context.getBean("userService");
 		System.out.println(service);
 		service.getUser("ycc", "123");
+	}
+	@Test
+	public void tsetProductService(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ProductService productService = (ProductService) context.getBean("productService");
+		System.out.println("productService"+productService.maxSize());
 	}
 }
